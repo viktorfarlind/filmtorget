7ad6298f4cc019e3e3d77fc56cb0cbd50b2d128e
Viktor <viktorfarlind@gmail.com>
2026-02-08 13:46:06 +0100

    Update CSP to allow WebSocket connections from Supabase

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,13 @@ const cspHeader = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
     style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: *.supabase.co;
+    img-src 'self' blob: data: https: *.supabase.co;
     font-src 'self';
     object-src 'none';
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'none';
-    connect-src 'self' *.supabase.co;
+    connect-src 'self' *.supabase.co wss://*.supabase.co;
 `.replace(/\s{2,}/g, ' ').trim();
 
 const nextConfig = {
