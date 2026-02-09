db12e8963dc4f6955f4d22feffd541006ab93fed
Viktor <viktorfarlind@gmail.com>
2026-02-09 18:59:50 +0100

    Remove Content Security Policy configuration from next.config.ts for simplification

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    connect-src 'self' https://*.supabase.co wss://*.supabase.co *.supabase.co;
-`
-  .replace(/\s{2,}/g, " ")
-  .trim();
-
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -23,19 +8,6 @@ const nextConfig = {
         hostname: "*.supabase.co",
       },
     ],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader,
-          },
-        ],
-      },
-    ];
   },
 };
 
